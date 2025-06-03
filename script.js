2b019aa7ab6df7b82d4b2fa07989d7495a282362
Micael Pimentel <p.micael.mp@gmail.com>
2025-06-03 12:33:41 -0300

    atualizaçao no tamnho do display de video

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function startVideoFromCam() {
   const specs = {
     video: {
       facingMode: "user", // Use "environment" for rear camera on mobile devices
-      width: { ideal: 1280 }, // Set ideal width
-      height: { ideal: 720 }, // Set ideal height
+      width: { ideal: 300 }, // Set ideal width
+      height: { ideal: auto}, // Set ideal height
     },
   };
   navigator.mediaDevices
